01db92204deee8ed3e2bcc3354788a392c562b92
Ajith27887 <ajithyogesh.n@gmail.com>
2025-06-05 00:10:10 +0530

    folder created and read dir for pdf file

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,54 @@
 import express from "express";
 import dotenv from "dotenv";
+import os from 'os'
+import path from "path";
+import fs from 'node:fs'
+import { log } from "node:console";
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
+const Home = os.homedir(),
+	dirname = path.dirname(Home),
+	Resume = path.join(Home +"/Downloads" +"/Resume"),
+	checkdir = fs.existsSync(Resume)
+
+	if (!checkdir) {
+		fs.mkdirSync(Resume)
+	}else{
+		console.log("folder already exists");
+	}
+
+	if (checkdir) {
+		fs.readdir(Resume, (err, files) => {
+	        if (err) {
+	            console.error('Error reading directory:', err);
+	            return;
+	        }
+	        if (files.length > 0) {
+	            console.log('Existing files:', files);
+				files.forEach((file) => {
+					if (path.extname(file) !== ".pdf") {
+						fs.unlink(path.join(Resume, file), (err) => {
+                    if (err) {
+                        console.error(`Error deleting file ${file}:`, err);
+                        return;
+                    }
+                    console.log(`Successfully deleted: ${file}`);
+                });
+					}
+				})
+				
+	        } else {
+	            console.log('No files in the directory');
+	        }
+	    });
+
+		fs.watch(Resume, { persistent: true }, (eventType, filename) => {
+			console.log(`Event type is: ${eventType}`);	
+		})
+	}
 
 app.listen(PORT, () => {
 	console.log(`port is connected ${process.env.PORT}`);
---
+++ backend/routes/folder.js
@@ -0,0 +1,7 @@
+import express from "express";
+import fs from 'fs/promises';
+import chokidar from 'chokidar';
+
+
+const app = express();
+
